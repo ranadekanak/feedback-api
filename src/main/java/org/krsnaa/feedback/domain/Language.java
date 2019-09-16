41c5f75ce4ff5be36011cec5c6b928a957b56e1b
Ranade <kranade@mcdermott.com>
2019-09-16 22:11:47 +0530

    table added for localization

--- src/main/java/org/krsnaa/feedback/domain/Language.java
+++ src/main/java/org/krsnaa/feedback/domain/Language.java
@@ -2,19 +2,15 @@ package org.krsnaa.feedback.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 @Entity
-@Table(name = "language")
-@EntityListeners(AuditingEntityListener.class)
-public class Language extends Audit {
+@Table(name = "localization")
+public class Language {
 
 	@Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
